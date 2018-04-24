882f16a66d1f6db4920818a0ca698eaeff750400
xiaoniu001 <1286204237@qq.com>
2018-04-24 21:59:11 +0800

    添加语言数据

--- data_analysis/parser.py
+++ data_analysis/parser.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from data_spider.spider import Spider
 from bs4 import BeautifulSoup
+from data_db.create_db import *
 
 
 class Parser(object):
@@ -8,15 +9,24 @@ class Parser(object):
     """数据解析类"""
 
     @staticmethod
-    def retrieve_data(url_response):
+    def retrieve_data(response_data):
         import time
-        soup = BeautifulSoup(url_response.text, "html5lib")
-        with open("../data_html/{0}.html".format(time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())), "w") as file:
-            file.write(url_response.text)
+        soup = BeautifulSoup(response_data.text, "html5lib")
+        with open("../data_html/index.html".format(time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())), "w") as file:
+            file.write(response_data.text)
         return soup
+
+    @classmethod
+    def language_data(cls, response_data):
+        languages = []
+        soup = cls.retrieve_data(response_data)
+        label_a = soup.find_all("a", "filter-item")
+        for label in label_a:
+            languages.append(Language(text="{}".format(label.contents[2])))
+        return languages
 
 
 if __name__ == '__main__':
     headers = {"Content-Type": "application/json;charset=utf-8"}
     response = Spider.request_url("https://github.com/search?utf8=%E2%9C%93&q=python&type=", headers=headers)
-    data_json = Parser.retrieve_data(response)
+    Parser.language_data(response)
---
+++ data_db/add_data.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+
+from data_db.create_db import session_scope
+
+
+class LanguageData(object):
+    """语言数据的增删改查"""
+
+    @classmethod
+    def add(cls):
+        pass
